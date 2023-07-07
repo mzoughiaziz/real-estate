9b9ca2617c5f56cebd86aa7d5be39503d74e5d90
Bader01 <baderrached65@gmail.com>
2023-07-07 16:36:55 +0100

    Add Load more for HouseList

--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -1,16 +1,15 @@
-import React, { useContext } from 'react';
-
-// import context
+import React, { useState, useContext } from 'react';
 import { HouseContext } from './HouseContext';
-// import components
 import House from './House';
-// import link
 import { Link } from 'react-router-dom';
-// import icons
 import { ImSpinner2 } from 'react-icons/im';
 
 const HouseList = () => {
   const { houses, loading } = useContext(HouseContext);
+  const [visibleHouses, setVisibleHouses] = useState(6); 
+  const loadMoreHouses = () => {
+    setVisibleHouses(visibleHouses + 6); 
+  };
 
   if (loading) {
     return (
@@ -30,7 +29,7 @@ const HouseList = () => {
     <section className='mb-20'>
       <div className='container mx-auto'>
         <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-14'>
-          {houses.map((house, index) => {
+          {houses.slice(0, visibleHouses).map((house, index) => {
             return (
               <Link to={`/property/${house.id}`} key={index}>
                 <House house={house} />
@@ -38,6 +37,16 @@ const HouseList = () => {
             );
           })}
         </div>
+        {visibleHouses < houses.length && (
+          <div className='text-center mt-6'>
+            <button
+              className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'
+              onClick={loadMoreHouses}
+            >
+              Mostrar mais
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
