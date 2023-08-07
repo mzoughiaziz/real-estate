923ef3c093941f34b7e37baf04db6ed143f4590e
Aziz Mzoughi <mzoughiaziz15@gmail.com>
2023-08-06 22:41:49 -0300

    Alignment adjusts on Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,11 +8,11 @@ const Banner = () => {
   return (
     <section className='h-full max-h-[640px] mb-8 xl:mb-24'>
       <div className='flex flex-col lg:flex-row'>
-        <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0'>
+        <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-end flex-1 px-4 lg:px-0'>
           <h1 className='text-4xl lg:text-[58px] font-semibold leading-none mb-6'>
             <span className='text-orangers'>Construa</span> Sua Casa Dos Sonhos Conosco.
           </h1>
-          <p className='max-w-[480px] mb-8'>
+          <p className='max-w-[580px] mb-8'>
             Com a Roberto Silveira o sonho de ter um lugar para chamar de seu está muito perto de se realizar! Temos diversas plantas disponíveis para você escolher o lugar onde você irá criar as memórias mais felizes com sua família. Mas se você deseja ir além, criamos um projeto do zero com base nas suas necessidades ou avaliamos o projeto que você já tem, para fazer do seu sonho realidade.
           </p>
           <div className='grid md:grid-cols-2 lg:grid-cols-2 gap-4 lg:gap-4'>
